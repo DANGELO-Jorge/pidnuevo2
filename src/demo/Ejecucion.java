b1eec30f537373d013169dcaa508fe3537a0e3e4
DANGELO-Jorge <secreto-99@hotmail.com>
2020-09-29 19:36:50 -0500

    se agrega variable m y q

--- src/demo/Ejecucion.java
+++ src/demo/Ejecucion.java
@@ -6,10 +6,14 @@ public class Ejecucion {
 		int x = 1;
 		int y = 2;
 		int z = 3;
+		int m =4;
+		int q = 5;
 		
 		System.out.println(x);
 		System.out.println(y);
 		System.out.println(z);
+		System.out.println(m);
+		System.out.println(q);
 		
 
 	}
